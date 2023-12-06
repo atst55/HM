3acb9966e74613751a15dc6044562f88866524b7
atst <149227944+atst55@users.noreply.github.com>
2023-12-06 12:03:20 -0800

    Add files via upload

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,10 +4,12 @@ extends CharacterBody2D
 signal player_fired_bullet(bullet, positon, direction)
 
 @export var bullet: PackedScene
-@export var speed: int = 200
+@export var speed: int = 100
 
 @onready var end_of_gun = $EndOfGun
 @onready var gun_direction = $GunDirection
+@onready var attack_cooldown = $AttackCooldown
+@onready var animation_player = $AnimationPlayer
 
 func _ready():
 	pass # Replace with function body.
@@ -18,9 +20,7 @@ func _ready():
 func _process(_delta:float):
 	var movement_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = movement_direction * speed
-	
 	move_and_slide()
-	Globals.player_pos = global_position
 	
 	look_at(get_global_mouse_position())
 	
@@ -30,8 +30,11 @@ func _unhandled_input(event: InputEvent):
 			
 	
 func shoot():
-	var bullet_instantiate = bullet.instantiate()
-	var direction = (gun_direction.global_position - end_of_gun.global_position).normalized()
-	emit_signal("player_fired_bullet", bullet_instantiate, end_of_gun.global_position, direction)
-
-
+	if attack_cooldown.is_stopped():
+		var bullet_instantiate = bullet.instantiate()
+		var direction = (gun_direction.global_position - end_of_gun.global_position).normalized()
+		emit_signal("player_fired_bullet", bullet_instantiate, end_of_gun.global_position, direction)
+		attack_cooldown.start()
+		animation_player.play("muzzle_flash")
+	
+	
---
+++ scenes/player/player_ui.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+@onready var life_bar = $MarginContainer/LifeBar
+
+func set_life_bar_max_value(max_value: int):
+	life_bar.max_value = max_value
+	
+func update_life_bar_value(life: int):
+	life_bar.value = life
